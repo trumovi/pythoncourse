e5e3122914bc80239615822cc46f5033f69789fe
trumovi <o.trumpf@g.nsu.ru>
2023-03-04 13:17:20 +0700

    Исправлено задание по json

--- json1.py
+++ json1.py
@@ -1,16 +1,16 @@
 import json
 
-defs = []
-with open('wikidata_1000.json', 'r', encoding="utf-8") as d:
-    for opr in d:
-        defs.append(json.loads(opr))
-
 info = {}
-for line in defs:
-    if "description" in line:
-        info[line["label"]["value"]] = line['description']["value"]
-    else:
-        info[line["label"]["value"]] = "None"
+with open('wikidata_1000.json', 'r', encoding="utf-8") as d:
+    for line in d:
+        defs = json.loads(line)
+        key = defs["label"]["value"]
+        if 'description' in line:
+            value = defs["description"]['value']
+        else:
+            value = None
+        info[key] = value
+
 
 with open("wiki.json", "w", encoding="utf-8") as g:
     json.dump(info, g, ensure_ascii=False, indent=4)
