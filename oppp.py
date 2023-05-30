999285ec4eee108430e3ff6152df7395069d9bd8
trumovi <o.trumpf@g.nsu.ru>
2023-05-30 17:54:28 +0700

    Исправила

--- oppp.py
+++ oppp.py
@@ -11,8 +11,9 @@ class TFIDF:
     def __init__(self, paths: list):
         self._text = []
         self._idf = {}
+        self._morph = pymorphy2.MorphAnalyzer()
         for item in paths:
-            self._add_file(item)
+            self._text.append(self._add_file(item)[1])
         if os.path.exists("idf.json"):
             with open('idf.json', 'r', encoding='UTF-8') as d:
                 self._idf = json.load(d)
@@ -21,15 +22,14 @@ class TFIDF:
                 self._count_idf(text)
 
     def _add_file(self, item):
-        morph = pymorphy2.MorphAnalyzer()
         with open(item, "r", encoding='UTF-8') as d:
             text = d.read()
         for symbol in '.,;:""?!-0123456789()—–«»':
             text = text.replace(symbol, '')
         w = text.lower().split()
-        words = [morph.parse(word)[0].normal_form for word in w]
+        words = [self._morph.parse(word)[0].normal_form for word in w]
         w_count = Counter(words)
-        self._text.append(w_count)
+        return words, w_count
 
     def _count_idf(self, text):
         for word in text:
@@ -39,24 +39,17 @@ class TFIDF:
                 if word in text:
                     di += 1
             self._idf[word] = math.log(abs(d) / abs(di))
-            with open('idf.json', 'w', encoding="utf-8") as d:
-                json.dump(self.get_idf(), d, ensure_ascii=False, indent=4)
+        with open('idf.json', 'w', encoding="utf-8") as d:
+            json.dump(self._idf, d, ensure_ascii=False, indent=4)
 
     def get_idf(self):
         return self._idf
 
-    def get_texts(self):
+    def get_texts(self):            # метод используется в тестах
         return self._text
 
     def count_tfidf(self, item):
-        morph = pymorphy2.MorphAnalyzer()
-        with open(item, "r", encoding='UTF-8') as d:
-            text = d.read()
-        for symbol in '.,;:""?!-0123456789()—–«»':
-            text = text.replace(symbol, '')
-        w = text.lower().split()
-        words = [morph.parse(word)[0].normal_form for word in w]
-        word_count = Counter(words)
+        words, word_count = self._add_file(item)
         tfidfs = []
         text_len = len(words)
         for word in word_count:
